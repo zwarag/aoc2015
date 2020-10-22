5797045afff69a7efeb9c08c09fa8a3fab4bc7ca
zwarag <harrys.kavan@protonmail.com>
2020-10-22 20:25:41 +0200

    day6

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import {Day1} from "./day1";
 import {Day2} from "./day2";
 import {Day3} from "./day3";
+import {Day4} from "./day4";
+import {Day5} from "./day5";
+import {Day6} from "./day6";
 
 // Day1.solve1();
 // Day1.solve2();
 // Day2.solve1();
 // Day2.solve2();
 // Day3.solve1();
-Day3.solve2();
+// Day3.solve2();
+// Day4.solve1();
+// Day4.solve2();
+// Day5.solve1();
+// Day5.solve2();
+// Day6.solve1();
+Day6.solve2();
